e14151fe160eaf95814549f1385514a09253c500
Mason <masonlopez@damotech.com>
2022-02-10 13:15:53 -0500

    With dist folder

---
+++ dist/types.d.ts
@@ -0,0 +1,13 @@
+export interface NSApiOptions {
+    consumerKey: string;
+    consumerSecret: string;
+    tokenId: string;
+    tokenSecret: string;
+    accountId: string;
+}
+export interface NSApiRequestOptions {
+    path?: string;
+    method: string;
+    body?: unknown;
+}
+export declare type NSRestletRequestOptions = NSApiRequestOptions;
